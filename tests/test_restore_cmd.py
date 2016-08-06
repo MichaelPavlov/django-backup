5549acab9123fdaad18dd407b7d1680860861f5d
Michael Pavlov <mihpavlov@gmail.com>
2016-08-06 16:25:57 +0300

    initial commit

--- tests/test_restore_cmd.py
+++ tests/test_restore_cmd.py
@@ -1,6 +1,7 @@
 import datetime
-from django.core.management import call_command
+
 from django.contrib.auth.models import User
+from django.core.management import call_command
 
 
 def test_full_roundtrip(db, tmpdir, settings, sftpserver):
